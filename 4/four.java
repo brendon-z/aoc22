8d7f42a23fc2c9e296d0c7fd3a57aec14b2236aa
brendon_z <brendon.zhao@gmail.com>
2022-12-04 22:20:37 +1100

    part 2 done

--- 4/four.java
+++ 4/four.java
@@ -6,13 +6,15 @@ public class four {
         File input = new File("input.txt");
         Scanner scanner = new Scanner(input);
  
-        int count = 0;
+        int containedCount = 0;
+        int overlapsCount = 0;
         while (scanner.hasNextLine()) {
             String pair = scanner.nextLine();
-            if (contains(pair)) count++;
+            if (contains(pair)) containedCount++;
+            if (overlaps(pair)) overlapsCount++;
         }
 
-        System.out.println("Overlaps: " + count);
+        System.out.println("Fully contained: " + containedCount + " , Total overlaps: " + overlapsCount);
         scanner.close();
     }
 
@@ -25,6 +27,20 @@ public class four {
         int second2nd = Integer.parseInt(second.split("-")[1]);
 
         if ((first1st <= second1st && first2nd >= second2nd) || (second1st <= first1st && second2nd >= first2nd)) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean overlaps(String pair) {
+        String first = pair.split(",")[0];
+        String second = pair.split(",")[1];
+        int first1st = Integer.parseInt(first.split("-")[0]);
+        int first2nd = Integer.parseInt(first.split("-")[1]);
+        int second1st = Integer.parseInt(second.split("-")[0]);
+        int second2nd = Integer.parseInt(second.split("-")[1]);
+
+        if (first1st <= second1st && first2nd >= second1st || second1st <= first1st && second2nd >= first1st) {
             return true;
         }
         return false;
