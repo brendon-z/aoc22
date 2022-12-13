dd92ba0942e0d35243c08dedef431c63f55c7c32
Brendon Zhao <brendon-z@Axiom-V3.localdomain>
2022-12-14 03:40:02 +1100

    done part1 of day 13 with a cheeky use of a null

---
+++ 13/13.js
@@ -0,0 +1,71 @@
+const fs = require("fs")
+
+const data = fs.readFileSync("input.txt", "utf-8")
+let lines = data.split(/\r?\n/)
+let sum = 0;
+
+for (let i = 0; i < lines.length; i++) {
+    if (i % 3 !== 0) {
+        continue
+    }
+    let line1 = lines[i]
+    let line2 = lines[i + 1]
+    console.log(lines[i])
+    console.log(lines[i + 1])
+
+    if (isValid(JSON.parse(line1), JSON.parse(line2))) {
+        console.log(`Pair ${Math.ceil((i + 2) / 3)} in order`)
+        sum += Math.ceil((i + 2) / 3)
+    } else {
+        console.log(`Pair ${Math.ceil((i + 2) / 3)} not in order`)
+    }
+    console.log()
+}
+
+console.log(sum)
+
+function isValid(json1, json2) {
+    console.log("Starting!")
+    console.log(json1, json2)
+    let json1Array
+    let json2Array
+
+    if (json1 === undefined || json2 === undefined) return false
+
+    if (!Array.isArray(json1)) {
+        if (!Array.isArray(json2)) {
+            return json1 < json2
+        }
+        json1Array = [json1]
+        json2Array = json2
+    } else if (!Array.isArray(json2)) {
+        json1Array = json1
+        json2Array = [json2]
+    } else {
+        json1Array = json1
+        json2Array = json2
+    }
+
+
+    for (let i = 0; i < json1Array.length; i++) {
+        if (isValid(json1Array[i], json2Array[i])) {
+            return true
+        } else if (isValid(json1Array[i], json2Array[i]) === null) {
+            continue
+        } else {
+            console.log(json1Array[i], json2Array[i])
+            console.log("no!")
+            if (json1Array[i] !== json2Array[i]) {
+                return false
+            }
+        }
+    }
+
+    if (json1Array.length < json2Array.length) {
+        return true
+    } else if (json1Array.length === json2Array.length) {
+        return null
+    }
+
+    return false
+}
